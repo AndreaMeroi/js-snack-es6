e2c31c067dd2bdf705949f58dfbc32a72e1a096d
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-28 17:13:23 +0200

    add array bike

---
+++ main.js
@@ -0,0 +1,41 @@
+//Snack 1
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.*/
+
+
+const bike = [
+    {
+        Bname: 'scott'
+        Bweight: 10
+    }
+    {
+        Bname: 'cipollini'
+        Bweight: 8
+    }
+    {
+        Bname: 'bianchi'
+        Bweight: 5
+    }
+    {
+        Bname: 'cannondale'
+        Bweight: 9
+    }
+    {
+        Bname: 'giant'
+        Bweight: 3
+    }
+    {
+        Bname: 'pinarello'
+        Bweight: 4
+    }
+    {
+        Bname: 'campagnolo'
+        Bweight: 15
+    }
+
+]
+
+/*Stampare a schermo la bici con peso minore.*/
+
+
