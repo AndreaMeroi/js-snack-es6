02882ca117fc5f924b08dc0236f4b13c8bdb9d5b
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-28 19:24:58 +0200

    add tutor support snack1

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ for (let i = 0; i < bike.length; i++) {
 
     const curBike = bike[i];
 
-    if (minWB.Bweight < bike[i]) {
+    if (curBike.Bweight < minWB.Bweight) {
         minWB = bike[i];
     }
 
