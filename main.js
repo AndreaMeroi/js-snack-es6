a7ac40530477106592a18da8efe858bad420029a
AndreaMeroi <andreameroidublin@gmail.com>
2025-07-29 16:47:32 +0200

    add half ex snack 2

--- main.js
+++ main.js
@@ -58,50 +58,64 @@ console.log('Snack 2')
 //Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 
-const teams = [
+let teams = [
     {
         Tname: 'Inter',
         points: 0,
-        failsDone: 0,
-        failsSuffered: 0
+        fails: 0
 
     },
     {
         Tname: 'Milan',
         points: 0,
-        failsDone: 0,
-        failsSuffered: 0
+        fails: 0
 
     },
     {
         Tname: 'Juve',
         points: 0,
-        failsDone: 0,
-        failsSuffered: 0
+        fails: 0
 
     },
     {
         Tname: 'Toro',
         points: 0,
-        failsDone: 0,
-        failsSuffered: 0
+        fails: 0
 
     },
     {
         Tname: 'Samp',
         points: 0,
-        failsDone: 0,
-        failsSuffered: 0
+        fails: 0
 
     }
+
 ]
 
+// inserire i numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”
+const newPoints = 'points';
+const newFails = 'fails';
 
-//Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+const newPointsNumb = randomNumb()
+const newFailssNumb = randomNumb()
 
 
+for (let i = 0; i < teams.length; i++) {
+
+    teams[i][newPoints] = newPointsNumb
+    teams[i][newFails] = newFailssNumb
 
 
+}
+
+console.log(teams)
+
+//creare una funzione per generare numeri random
+
+
+function randomNumb(min, max) {
+    return Math.floor(Math.random() * (50 - 0 + 1) + 0);
+}
 
 
 
